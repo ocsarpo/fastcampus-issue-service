1910a8ba1fca19372283bfbe4fff2bf875b219fb
ocsarpo <ocsarpo22@gmail.com>
2022-08-01 00:39:33 +0900

    initial

---
+++ src/main/kotlin/com/fastcampus/FastcampusIssueServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.fastcampus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastcampusIssueServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FastcampusIssueServiceApplication>(*args)
+}
