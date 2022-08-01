8bba85e99db4d18efa3f4dd820b6a29a27d4361b
ocsarpo <ocsarpo22@gmail.com>
2022-08-02 00:30:46 +0900

    08. 이슈 수정

--- issue-service/src/main/kotlin/fastcampus/issueservice/service/IssueService.kt
+++ issue-service/src/main/kotlin/fastcampus/issueservice/service/IssueService.kt
@@ -3,8 +3,10 @@ package fastcampus.issueservice.service
 import fastcampus.issueservice.domain.IssueEntity
 import fastcampus.issueservice.domain.IssueEntityRepository
 import fastcampus.issueservice.domain.enums.IssueStatus
+import fastcampus.issueservice.exception.NotFoundException
 import fastcampus.issueservice.model.IssueRequest
 import fastcampus.issueservice.model.IssueResponse
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -33,4 +35,29 @@ class IssueService(
     fun getAll(status: IssueStatus) =
         issueEntityRepository.findAllByStatusOrderByCreatedAtDesc(status)
             .map { IssueResponse(it) }
+
+    @Transactional(readOnly = true)
+    fun get(id: Long): IssueResponse {
+        val issueEntity = issueEntityRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈가 존재하지 않습니다")
+
+        return IssueResponse(issueEntity)
+    }
+
+    @Transactional
+    fun edit(userId: Long, id: Long, request: IssueRequest): IssueResponse {
+        val issueEntity = issueEntityRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈가 존재하지 않습니다")
+
+        return with(issueEntity) {
+            summary = request.summary
+            description = request.description
+            this.userId = userId
+            type = request.type
+            priority = request.priority
+            status = request.status
+
+            // 더티체킹 하기 때문에 save 를 호출 하지 않아도 되지만, 명시적으로 사용함.
+            IssueResponse(issueEntityRepository.save(this))
+        }
+    }
+
 }
--- issue-service/src/main/kotlin/fastcampus/issueservice/web/IssueController.kt
+++ issue-service/src/main/kotlin/fastcampus/issueservice/web/IssueController.kt
@@ -5,7 +5,9 @@ import fastcampus.issueservice.domain.enums.IssueStatus
 import fastcampus.issueservice.model.IssueRequest
 import fastcampus.issueservice.service.IssueService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -28,4 +30,17 @@ class IssueController(
         authUser: AuthUser,
         @RequestParam(required = false, defaultValue = "TODO") status: IssueStatus,
     ) = issueService.getAll(status)
+
+    @GetMapping("/{id}")
+    fun get(
+        authUser: AuthUser, // 유효한 회원만 요청 가능하도록 하기 위해 작성함.
+        @PathVariable id: Long,
+    ) = issueService.get(id) // 타 유저의 것도 조회가능하므로 authUser.id 를 넘기지 않음.
+
+    @PutMapping("/{id}")
+    fun edit(
+        authUser: AuthUser,
+        @PathVariable id: Long,
+        @RequestBody request: IssueRequest,
+    ) = issueService.edit(authUser.userId, id, request)
 }
